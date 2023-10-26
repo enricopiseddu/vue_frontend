b9f7a701b65372c06103e70b848bedd5d18b7966
martinasalis <marti.salis20@gmail.com>
2023-10-26 12:55:28 +0200

    Added css in Login page

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -4,28 +4,25 @@
       <h1>Login</h1>
       <div id="alert" v-if="alert">{{ alert }}</div>
   
-      <form @submit.prevent="onSubmit">
+      <form @submit.prevent="onSubmit" class="bg-white shadow-md rounded px-3 pt-6 pb-8 mb-4">
         
-        <div>
-            <label>
-              Username
-              <input type="text" v-model="username" />
-            </label>
+        <div class="mx-auto">
+          <div class="mt-2">
+            <label class="me-auto text-gray-700 text-sm font-bold mb-2"> Username </label>
+          </div>
+            
+          <input type="text" v-model="username" class="mt-4 shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"/>
+            
         </div>
 
-        <br>
-
-        <div>
-          <label>
-            Password
-            <input type="password" v-model="password" />
-          </label>
+        <div class="mx-auto">
+          <div class="mt-2">
+            <label class="me-auto text-gray-700 text-sm font-bold mb-2"> Password </label>
+          </div>
+          <input type="password" v-model="password" class="mt-4 shadow appearance-none border rounded  py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"/>
         </div>
         
-        <br>
-        
-  
-        <button type="submit">Log in</button>
+        <button type="button" class="mt-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Log in</button>
       </form>
   
       <div>
