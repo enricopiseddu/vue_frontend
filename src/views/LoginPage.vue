9d43498498b419bfee4d4c18b65d9841cf8306a7
martinasalis <marti.salis20@gmail.com>
2023-11-03 14:50:16 +0100

    Added class css and update gitignore

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -19,7 +19,7 @@
             <input type="password" v-model="password" class="mt-4 shadow appearance-none border rounded  py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"/>
           </div>
           
-          <button type="submit" class="mt-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 focus:outline-none dark:focus:ring-blue-800">Log in</button>
+          <button type="submit" class="button" >Log in</button>
         </form>
       </div>
       
